59cd74ee37e032d7384f397e3d06c76ccb039a91
Jake <jhuggart@gmail.com>
2014-08-05 20:24:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LimeApp::Application.config.secret_token = '13e86a39642c610da520d50d283915764ec265e60d8f8409af5d3febc53237b0941cca5d5914810d22a27327f73e34672cfb61045ab33827c9a23ce67a3db9dc'
